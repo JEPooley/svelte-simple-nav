698cfb2aac37b35631be98bd4499bbd8df3e9fb0
Josh Pooley <joshpooley93@gmail.com>
2022-08-27 07:58:52 +0100

    refactor typescript

--- src/lib/useMediaQuery.ts
+++ src/lib/useMediaQuery.ts
@@ -1,9 +1,9 @@
 // Credit: https://medium.com/@ricciutipaolo/how-to-check-for-media-queries-in-svelte-with-usemediaquery-604f14190035
 import { readable, type Readable } from 'svelte/store';
 
-export const useMediaQuery = (mediaQueryString: string) => {
-    const matches: Readable<boolean> = readable(true, (set) => {
-        const mql: MediaQueryList = window.matchMedia(mediaQueryString);
+export const useMediaQuery = (mediaQueryString: string): Readable<boolean> => {
+    const matches = readable(true, (set) => {
+        const mql = window.matchMedia(mediaQueryString);
         set(mql.matches);
         const el = (e: MediaQueryListEvent) => set(e.matches);
         mql.addEventListener("change", el);
